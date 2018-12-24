7ac314cd432346402d798f00e50200f209ae58be
NikSS <nik.safe@gmail.com>
2018-12-24 10:06:44 -0800

    post route to twilio

--- routes/sms.js
+++ routes/sms.js
@@ -1,12 +1,33 @@
 const express = require('express');
 
-const router = express.Router();
 
 const Twilio = require('twilio');
 const dotEnv = require('dotenv');
+
+dotEnv.config();
+const router = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_Number;
+const client = new Twilio(accountSid, authToken);
+
+// This is the message to send to clients
+const createApptMsg = (doctorName, patientName, date, time, customMsg) => (
+  `Hello ${patientName},\nThis is Dr. ${doctorName}'s office reminding you about your appointment on ${date} at ${time}.${customMsg ? `\nNOTE: ${customMsg}` : ''}`
+);
+
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.post('/', (req, res) => {
+  const {
+    phoneNumber, doctorName, patientName, date, time, customMsg,
+  } = req.body;
+  const apptMessage = createApptMsg(doctorName, patientName, date, time, customMsg);
+
+  client.messages.creat({
+    body: apptMessage,
+    to: phoneNumber,
+    from: twilioNumber,
+  });
 });
 
 module.exports = router;
